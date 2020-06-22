b52da0391fe1fb8609eae25e42471fa7b534e880
akshat0047 <pande.akshat21@gmail.com>
2020-06-22 12:54:33 +0530

    added commands to create superuser, migrations and collectstatic

--- docker/Files/Docker-script.sh
+++ docker/Files/Docker-script.sh
@@ -4,4 +4,7 @@ chown -R apache /Sites/online_test/yaksh
 chown -R nobody /Sites/online_test/yaksh_data
 chmod -R 664 /Sites/online_test
 chmod -R +X /Sites
+python3 manage.py migrate --noinput
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin','admin@example.com','admin')"|python3 manage.py shell
+python3 manage.py collectstatic --noinput
 /usr/sbin/httpd -D FOREGROUND
